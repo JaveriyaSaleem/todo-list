617cf76fa91715a5d3000b247ea347d5356d615d
JaveriyaSaleem <saleemjaveriya2@gmail.com>
2024-09-01 02:52:41 +0500

    Your commit message

---
+++ app.js
@@ -0,0 +1,70 @@
+
+var inputValue = document.getElementById('input')
+
+
+function addTask(){
+  var createRow = document.createElement('div');
+  createRow.className = 'row'; //row created
+  createRow.classList.add('row', 'd-inline-flex', 'align-items-center', 'border', 'border-0', 'rounded-pill', 'bg-lightdark', 'p-1', 'mb-2');
+var containerValue = document.getElementById('containerOfTask')
+containerValue.appendChild(createRow)
+createRow.innerHTML = `<div class="col-auto d-flex align-items-center fs-4">`+inputValue.value+`</div>
+            <div class="col-auto d-flex align-items-center justify-content-center gap-2">
+                <i class="fa-solid fa-check task-button fs-5 p-1 rounded-circle" style="color: #000000;" onclick="completed(this)"></i>
+                <i class="fa-solid fa-pen-to-square task-button fs-5 p-1 rounded-circle" style="color: #000000;" onclick="editText(this)"></i>
+                <i class="fa-solid fa-trash task-button fs-5 p-1 rounded-circle" style="color: #000000;" onclick="dltIcon(this)"></i>
+            </div>`
+  console.log(createRow)
+
+  // emptying the input field 
+inputValue.value = "";
+}
+// deleteAll 
+function deleteAll(){
+    lists.innerHTML = ""
+  }
+// edit icon -->
+function editText(element) {
+  Swal.fire({
+    input: 'textarea',
+    inputLabel: 'Message',
+    inputPlaceholder: 'Type your message here...',
+    inputAttributes: {
+      'aria-label': 'Type your message here'
+    },
+    showCancelButton: true,
+    inputValidator: (value) => {
+      if (!value) {
+        return 'You need to write something!';
+      }
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Store the input value
+      const text = result.value;
+
+ 
+      console.log(text); // Outputs the input value to the console
+      var getTaskValue = element.parentNode.previousElementSibling;
+      getTaskValue.innerHTML = text
+      window.storedText = text; // Global variable
+      localStorage.setItem('storedText', text); // Local storage example
+    }
+  });
+}
+// completed icon
+function completed(element){
+  var fromIconToTask = element.parentNode.previousElementSibling;
+  fromIconToTask.classList.add('text-decoration-line-through')
+
+  
+}
+// delete icon
+function dltIcon(element){
+  var taskElement = element.parentNode.parentNode
+  taskElement.classList.add('animate__animated','animate__backOutDown')
+  setTimeout( function() {
+    taskElement.remove
+  }, 2000);    
+ }
+
